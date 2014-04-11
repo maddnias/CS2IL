fa4bb4d961ada5a153850415d6f4df8cfe173f09
UbbeLoL <ubbelol@ubbelol-VirtualBox.(none)>
2014-04-11 16:16:21 +0200

    prepare for opensource

--- server.js
+++ server.js
@@ -16,17 +16,18 @@ app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/index', function(req, res) {
+app.get('/cs2il', function(req, res) {
 	res.render("index.html");
 });
 
 app.get('/', function(req, res) {
-	res.redirect('/index');
+	res.redirect('/cs2il');
 });
 
 var d = require('domain').create();
 
 d.on('error', function(err) {
+	//i know..
 	console.log(err);
 });
 
@@ -37,35 +38,21 @@ app.post('/index', function(req, res){
 		        console.log(err);
 		    } else {
 		        var output = JSON.parse(execSync("./files/CS2ILHelper.exe ./files/test ./files/tmp123.exe " + req.body.version + " " + req.body.cbComments));
-
-		        var disasm = output["Disassembly"].substring(1, output["Disassembly"].length-1);
-		        disasm = disasm.split('\\n');
-				var codeMap = output["CodeMap"];
-				var source = req.body.txtCode.split(endOfLine);
-
-				var codeBlocks = [];
-				var ilBlocks = [];
-				var codeMaps = [];
+		        var source = req.body.txtCode.split(endOfLine);
+		        var codeBlocks = [];
 
 				for(var i = 0;i < source.length;i++) {
-					codeBlocks.push({index: i, block:source[i]});
+					codeBlocks.push({
+						index: i,
+						block:source[i]
+					});
 				}
 
-				for(var i = 0;i < disasm.length;i++) {
-					ilBlocks.push({index: i, block:disasm[i].split('|')[0].replace("\\t", "\t"), ilidx: disasm[i].split('|')[1]});
-				}
-
-				for(var i = 1, x = 1;i < codeMap.split('_').length;i+=2,x++) {
-					codeMaps.push({line:codeMap.split('_')[i], indexes: codeMap.split('_')[i+1].split('|')[1].replace('"', '')});
-				}
-
-		    	res.render('paste.html', { cscode: codeBlocks, ilcode: ilBlocks, codemaps: codeMaps});
-		    	var ip = req.headers['x-forwarded-for'] || 
-    					req.connection.remoteAddress || 
-     					req.socket.remoteAddress ||
-     					req.connection.socket.remoteAddress;
-		    	fs.appendFile('log.txt', ip + ' just compiled a file!\r\n', function (err) {
-				});
+		        res.render('paste.html', {
+		        							"cscode" : codeBlocks,
+		        							"ilcode" : output['Disassembly'],
+		        							"codemap" : output['CodeMap'] 
+		        						 });
 		    }
 		}); 
 	});
