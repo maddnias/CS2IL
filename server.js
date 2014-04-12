7301df7de401d483d1a6a026da0d1472d54cb527
UbbeLoL <ubbelol@ubbelol-VirtualBox.(none)>
2014-04-12 13:35:39 +0200

    fix exploits

--- server.js
+++ server.js
@@ -37,7 +37,21 @@ app.post('/index', function(req, res){
 		    if(err) {
 		        console.log(err);
 		    } else {
-		        var output = JSON.parse(execSync("./files/CS2ILHelper.exe ./files/test ./files/tmp123.exe " + req.body.version + " " + req.body.cbComments));
+		    	var comments = req.body.cbComments == "on";
+
+		    	if(req.body.version[0] != "2" &&
+		    		req.body.version[0] != "3" &&
+		    		req.body.version[0] != "4")
+		    		res.send('Invalid data');
+
+		    	if(req.body.version[1] != "0" &&
+		    		req.body.version[1] != "5")
+		    		res.send('Invalid data');
+
+		    	if(req.body.version.length != 2)
+		    		res.send('Invalid data');
+
+		        var output = JSON.parse(execSync("./files/CS2ILHelper.exe ./files/test ./files/tmp123.exe " + req.body.version + " " + (comments ? "on" : "off")));
 		        var source = req.body.txtCode.split(endOfLine);
 		        var codeBlocks = [];
 
